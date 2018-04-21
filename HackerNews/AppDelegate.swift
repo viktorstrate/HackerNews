d969b91d7c560cc7eedc47d898a905e51316e31e
viktorstrate <viktorstrate@gmail.com>
2018-04-21 16:09:45 +0200

    Initial Commit

---
+++ HackerNews/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HackerNews
+//
+//  Created by Viktor Hundahl Strate on 21/04/2018.
+//  Copyright © 2018 Viktor Hundahl Strate. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HackerNews/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HackerNews
+//
+//  Created by Viktor Hundahl Strate on 21/04/2018.
+//  Copyright © 2018 Viktor Hundahl Strate. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
